560da8797fddeaa49e1ad6cd1dc5533781299694
Viktorija Ravaite <viktorijagalinskaite@gmail.com>
2022-07-25 07:14:37 +0300

    [Homework]-test automation executed

--- homework/tests/example.spec.js
+++ homework/tests/example.spec.js
@@ -17,7 +17,7 @@ const data = [
 
 data.forEach(version => {
   test.describe(version + ': Add', () => {
-    test('Concatenating 2 and 3 results in 23', async ({ page }) => {
+    test('Add 2 and 3 results in 5', async ({ page }) => {
       await page.goto('https://testsheepnz.github.io/BasicCalculator');
       await page.selectOption('#selectBuild', { label: version});
       await page.locator('#number1Field').type('2');
@@ -28,4 +28,149 @@ data.forEach(version => {
       await expect(page.locator('#numberAnswerField')).toHaveValue('5');
     });
   });
+
+  test.describe(version + ': Subtract', () => {
+    test('Subtract 3 and 2 results in 1', async ({ page }) => {
+      await page.goto('https://testsheepnz.github.io/BasicCalculator');
+      await page.selectOption('#selectBuild', { label: version});
+      await page.locator('#number1Field').type('3');
+      await page.locator('#number2Field').type('2');
+      await page.selectOption('#selectOperationDropdown', {label: 'Subtract'});
+      await page.locator('#calculateButton').click();
+  
+      await expect(page.locator('#numberAnswerField')).toHaveValue('1');
+    });
+  });
+
+  test.describe(version + ': Multiply', () => {
+    test('Multiply 3 and 2 results in 6', async ({ page }) => {
+      await page.goto('https://testsheepnz.github.io/BasicCalculator');
+      await page.selectOption('#selectBuild', { label: version});
+      await page.locator('#number1Field').type('3');
+      await page.locator('#number2Field').type('2');
+      await page.selectOption('#selectOperationDropdown', {label: 'Multiply'});
+      await page.locator('#calculateButton').click();
+  
+      await expect(page.locator('#numberAnswerField')).toHaveValue('6');
+    });
+  });
+
+  test.describe(version + ': Divide', () => {
+    test('Divide 6 by 2 results in 3', async ({ page }) => {
+      await page.goto('https://testsheepnz.github.io/BasicCalculator');
+      await page.selectOption('#selectBuild', { label: version});
+      await page.locator('#number1Field').type('6');
+      await page.locator('#number2Field').type('2');
+      await page.selectOption('#selectOperationDropdown', {label: 'Divide'});
+      await page.locator('#calculateButton').click();
+  
+      await expect(page.locator('#numberAnswerField')).toHaveValue('3');
+    });
+  });
+
+  test.describe(version + ': Concatenate', () => {
+    test('Concatenate 6 and 2 results in 62', async ({ page }) => {
+      await page.goto('https://testsheepnz.github.io/BasicCalculator');
+      await page.selectOption('#selectBuild', { label: version});
+      await page.locator('#number1Field').type('6');
+      await page.locator('#number2Field').type('2');
+      await page.selectOption('#selectOperationDropdown', {label: 'Concatenate'});
+      await page.locator('#calculateButton').click();
+  
+      await expect(page.locator('#numberAnswerField')).toHaveValue('62');
+    });
+  });
+
+  test.describe(version + ': Divide', () => {
+    test('Divide 3 by 2 results in 1 as integers only chosen', async ({ page }) => {
+      await page.goto('https://testsheepnz.github.io/BasicCalculator');
+      await page.selectOption('#selectBuild', { label: version});
+      await page.locator('#number1Field').type('3');
+      await page.locator('#number2Field').type('2');
+      await page.selectOption('#selectOperationDropdown', {label: 'Divide'});
+      await page.locator('#integerSelect').click();
+      await page.locator('#calculateButton').click();
+  
+      await expect(page.locator('#numberAnswerField')).toHaveValue('1');
+    });
+  });
+
+  test.describe(version + ': Add', () => {
+    test('Add 6.5 and 2 results in 8.5', async ({ page }) => {
+      await page.goto('https://testsheepnz.github.io/BasicCalculator');
+      await page.selectOption('#selectBuild', { label: version});
+      await page.locator('#number1Field').type('6.5');
+      await page.locator('#number2Field').type('2');
+      await page.selectOption('#selectOperationDropdown', {label: 'Add'});
+      await page.locator('#calculateButton').click();
+  
+      await expect(page.locator('#numberAnswerField')).toHaveValue('8.5');
+    });
+  });
+
+  test.describe(version + ': Add', () => {
+    test('Add -1 and 2 results in 1', async ({ page }) => {
+      await page.goto('https://testsheepnz.github.io/BasicCalculator');
+      await page.selectOption('#selectBuild', { label: version});
+      await page.locator('#number1Field').type('-1');
+      await page.locator('#number2Field').type('2');
+      await page.selectOption('#selectOperationDropdown', {label: 'Add'});
+      await page.locator('#calculateButton').click();
+  
+      await expect(page.locator('#numberAnswerField')).toHaveValue('1');
+    });
+  });
+
+  test.describe(version + ': Concatenate', () => {
+    test('Add 6.5 and 2 results in 6.52', async ({ page }) => {
+      await page.goto('https://testsheepnz.github.io/BasicCalculator');
+      await page.selectOption('#selectBuild', { label: version});
+      await page.locator('#number1Field').type('6.5');
+      await page.locator('#number2Field').type('2');
+      await page.selectOption('#selectOperationDropdown', {label: 'Concatenate'});
+      await page.locator('#calculateButton').click();
+  
+      await expect(page.locator('#numberAnswerField')).toHaveValue('6.52');
+    });
+  });
+
+  test.describe(version + ': Concatenate', () => {
+    test('Add 1 and -2 results in 1-2', async ({ page }) => {
+      await page.goto('https://testsheepnz.github.io/BasicCalculator');
+      await page.selectOption('#selectBuild', { label: version});
+      await page.locator('#number1Field').type('1');
+      await page.locator('#number2Field').type('-2');
+      await page.selectOption('#selectOperationDropdown', {label: 'Concatenate'});
+      await page.locator('#calculateButton').click();
+  
+      await expect(page.locator('#numberAnswerField')).toHaveValue('1-2');
+    });
+  });
+
+    test.describe(version + ': Add', () => {
+      test('Add A and 1 results in error message', async ({ page }) => {
+        await page.goto('https://testsheepnz.github.io/BasicCalculator');
+        await page.selectOption('#selectBuild', { label: version});
+        await page.locator('#number1Field').type('A');
+        await page.locator('#number2Field').type('1');
+        await page.selectOption('#selectOperationDropdown', {label: 'Add'});
+        await page.locator('#calculateButton').click();
+    
+        await expect(page.locator('#errorMsgField')).toHaveText('Number 1 is not a number');
+      });
+    });
+
+    test.describe(version + ': Concatenate', () => {
+      test('Under selection Concatenate there is no box for integers only', async ({ page }) => {
+        await page.goto('https://testsheepnz.github.io/BasicCalculator');
+        await page.selectOption('#selectBuild', { label: version});
+        await page.locator('#number1Field').type('3');
+        await page.locator('#number2Field').type('2');
+        await page.selectOption('#selectOperationDropdown', {label: 'Concatenate'});
+        await page.locator('#intSelectionLabel').isHidden();
+        await page.locator('#calculateButton').click();
+    
+        await expect(page.locator('#numberAnswerField')).toHaveValue('32');
+      });
+    });
 });
